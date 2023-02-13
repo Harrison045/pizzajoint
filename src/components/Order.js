323fcb2f55ad78d92a9f0e67ae879c293c87973f
Harrison045 <harrisonprince324@gmail.com>
2023-02-13 15:14:36 +0000

    done

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,30 +1,29 @@
-import React, { useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import React, { useState,useEffect } from "react";
+import { motion } from "framer-motion";
 
 const containerVariants = {
-  hidden: { 
-    opacity: 0, 
-    x: '100vw',
+  hidden: {
+    opacity: 0,
+    x: "100vw",
     transition: {
       staggerChildren: 0.5,
-    } 
+    },
   },
-  visible: { 
-    opacity: 1, 
+  visible: {
+    opacity: 1,
     x: 0,
-    transition: { 
-      type: 'spring',
+    transition: {
+      type: "spring",
       mass: 0.4,
       damping: 8,
       staggerChildren: 0.4,
       when: "beforeChildren",
-    }
+    },
   },
-  exit:{
-    x:'-100vw',
-    transition:{ease: 'easeInOut'}
-      
-    }
+  exit: {
+    x: "-100vw",
+    transition: { ease: "easeInOut" },
+  },
 };
 
 const childVariants = {
@@ -33,34 +32,35 @@ const childVariants = {
   },
   visible: {
     opacity: 1,
-  }
-}
+  },
+};
 
-const Order = ({ pizza }) => {
-  const [showTitle, setShowTitle] = useState(true);
-  setTimeout(() => {
-    setShowTitle(false);
-  }, 4000);
-
+const Order = ({ pizza, setShowModal }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setShowModal(true);
+    }, 5000);
+  }, [setShowModal]);
   return (
-    <motion.div className="container order"
+    <motion.div
+      className="container order"
       variants={containerVariants}
       initial="hidden"
       animate="visible"
       exit="exit"
     >
-      <AnimatePresence>
-        {showTitle && (
-          <motion.h2 exit={{ y: -1000 }}>Thank you for your order :)</motion.h2>
-        )}
-      </AnimatePresence>
-      <motion.p variants={childVariants}>You ordered a {pizza.base} pizza with:</motion.p>
+      <h2>Thank you for your order :)</h2>
+
+      <motion.p variants={childVariants}>
+        You ordered a {pizza.base} pizza with:
+      </motion.p>
       <motion.div variants={childVariants}>
-        {pizza.toppings.map(topping => <div key={topping} >{topping}</div>)}
+        {pizza.toppings.map((topping) => (
+          <div key={topping}>{topping}</div>
+        ))}
       </motion.div>
-      
     </motion.div>
-  )
-}
+  );
+};
 
 export default Order;
